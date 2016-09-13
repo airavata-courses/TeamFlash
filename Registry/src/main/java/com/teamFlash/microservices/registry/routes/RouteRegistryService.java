c522100b53388ec0548e550d0dcbbb8c47d6f8cd
Debasis Dwivedy <ddwivedy@iu.edu>
2016-09-13 06:52:46 -0400

    Registry Main Module

---
+++ Registry/src/main/java/com/teamFlash/microservices/registry/routes/RouteRegistryService.java
@@ -0,0 +1,17 @@
+package com.teamFlash.microservices.registry.routes;
+
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.builder.RouteBuilder;
+
+import com.teamFlash.microservices.registry.services.RegistryService;
+
+
+public class RouteRegistryService extends RouteBuilder{
+
+	public void configure() throws Exception {
+		System.out.println("in RouteRegistryService.configure ---");
+		from("direct:getAudit").setExchangePattern(ExchangePattern.InOut).bean(new RegistryService(), "getAudit");
+	    // add HTTP interface
+	    from("jetty:http://0.0.0.0:{{port}}/audit").setExchangePattern(ExchangePattern.InOut).to("direct:getAudit");
+	}
+}
---
+++ Registry/src/main/java/com/teamFlash/microservices/registry/services/RegistryService.java
@@ -0,0 +1,11 @@
+package com.teamFlash.microservices.registry.services;
+
+public class RegistryService {
+
+	public String getAudit()
+	{
+		String audit="in audit";
+		return audit;
+	}
+	
+}
