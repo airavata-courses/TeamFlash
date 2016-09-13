d6b372f84de4b5ad61bc9d90676ac26e0bf179d7
Debasis Dwivedy <ddwivedy@iu.edu>
2016-09-13 05:45:23 -0400

    Data Ingestor

--- DataIngestor/src/main/java/com/teamFlash/microservices/dataIngestor/app/RunDataIngestor.java
+++ DataIngestor/src/main/java/com/teamFlash/microservices/dataIngestor/app/RunDataIngestor.java
@@ -1,10 +1,33 @@
 package com.teamFlash.microservices.dataIngestor.app;
 
+import org.apache.camel.main.Main;
+
+import com.teamFlash.microservices.dataIngestor.routes.DataIngestorRouteBuilder;
+
+
 public class RunDataIngestor {
 
-	public static void main(String[] args) {
+	private Main main;
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		RunDataIngestor instance=new RunDataIngestor();
+		final String port = (args.length == 1 ? args[0] : "8765");
+		instance.boot(port);
 	}
 
+	@SuppressWarnings("deprecation")
+	public void boot(String port) throws Exception {
+	    System.setProperty("port", port);
+
+	    // create a Main instance
+	    main = new Main();
+	    // enable hangup support so you can press ctrl + c to terminate the JVM
+	    main.enableHangupSupport();
+	    // add routes
+	    main.addRouteBuilder(new DataIngestorRouteBuilder());
+
+	    // run until you terminate the JVM
+	    System.out.println(String.format("Starting Camel, using port %s. Use ctrl + c to terminate the JVM.", port));
+	    main.run();
+	  }
 }
