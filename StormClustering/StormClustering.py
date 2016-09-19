97b9748755bb50d0c25ab2967f44ad72c57d980b
akmehra <akmehra@iu.edu>
2016-09-19 05:31:07 -0400

    CI/CD python

--- StormClustering/StormClustering.py
+++ StormClustering/StormClustering.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    current_dummy_data = "Random KML file data. Will contain data generated from Strom Clustering later in kml format"
+    current_dummy_data = "Random KML file data.Will contain data generated from Strom Clustering later in kml format"
     response = make_response(current_dummy_data)
     response.headers["Content-Disposition"] = "attachment; filename=data.kml"
     return response
