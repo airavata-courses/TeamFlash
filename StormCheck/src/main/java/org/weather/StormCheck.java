e61827d5e36275b212059eb7d39086348612bdb3
Girish Gabra <girish.gabra10@gmail.com>
2016-09-24 18:30:56 -0400

    Basic code for forecast trigger

---
+++ StormCheck/src/main/java/org/weather/StormCheck.java
@@ -0,0 +1,25 @@
+package org.weather;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by girish on 9/18/16.
+ */
+@Path("/verify")
+public class StormCheck {
+
+    public String isStormPresent = "{\"message\": \"Yes\"}";
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String checkStormExists(@QueryParam("value") boolean exists )
+    {
+
+        System.out.println(exists);
+        if(!exists){
+            isStormPresent = "{\"message\": \"No\"}";
+        }
+        return isStormPresent;
+    }
+}
