a1a4f8d9361da8b6178263a2b7290cf550a778a7
Pranav <pmpande@iu.edu>
2016-09-24 18:21:49 -0400

    Added detectClusters method which sends cluster data in json format

--- StormClustering.py
+++ StormClustering.py
@@ -1,13 +1,24 @@
 from flask import Flask
-from flask import make_response
+from flask import Response
+from flask import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    current_dummy_data = "Random KML file data.Will contain data generated from Strom Clustering later in kml format!"
-    response = make_response(current_dummy_data)
-    response.headers["Content-Disposition"] = "attachment; filename=data.kml"
+def detectClusters():
+    clusters = {
+        "data" : [
+            {"longitude":"-112.0822680013139","latitude":"36.09825589333556","altitude":"0",
+             "heading":"103.8120432044965","tilt":"62.04855796276328","range":"2889.145007690472"},
+            {"longitude": "-112.0822680013139", "latitude": "36.09825589333556", "altitude": "0",
+             "heading": "103.8120432044965", "tilt": "62.04855796276328", "range": "2889.145007690472"},
+            {"longitude": "-112.0822680013139", "latitude": "36.09825589333556", "altitude": "0",
+             "heading": "103.8120432044965", "tilt": "62.04855796276328", "range": "2889.145007690472"}
+        ]
+    }
+
+    js = json.dumps(clusters)
+    response = Response(js, status=200, mimetype='application/json')
     return response
 
 
