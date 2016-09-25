072537c3f889cd81df80199cf7f2049382df6a92
Pranav <pmpande@iu.edu>
2016-09-24 22:27:18 -0400

    Updated detectStorm function

--- StormDetection.py
+++ StormDetection.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask import make_response
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    current_dummy_data = "Random KML file data.Will contain data generated from Strom Clustering later in kml format!"
-    response = make_response(current_dummy_data)
+@app.route('/detectStorm/<year>/<month>/<day>/<station>/<filename>', methods=['GET'])
+def detectStormLocation(year=None, month=None, day=None, station=None, filename=None):
+    url = 'https://noaa-nexrad-level2.s3.amazonaws.com/' + year + '/' + month + '/' + day + '/' + station + '/' + filename + '.gz'
+    #url = 'https://noaa-nexrad-level2.s3.amazonaws.com/1999/04/05/KAMX/KAMX19990405_000408.gz'
+    data = urlopen(url).read()
+    response = make_response(data)
     response.headers["Content-Disposition"] = "attachment; filename=data.kml"
     return response
 
