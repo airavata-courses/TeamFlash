c476ad3c3233323283bfaf5e5c217e8aa589f775
Pranav <pmpande@iu.edu>
2016-09-25 00:15:43 -0400

    Added dummy kml file and logic to serve it

--- StormDetection.py
+++ StormDetection.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask import make_response
 from urllib.request import urlopen
+import os
 
 app = Flask(__name__)
 
 @app.route('/detectStorm/<year>/<month>/<day>/<station>/<filename>', methods=['GET'])
 def detectStormLocation(year=None, month=None, day=None, station=None, filename=None):
     url = 'https://noaa-nexrad-level2.s3.amazonaws.com/' + year + '/' + month + '/' + day + '/' + station + '/' + filename + '.gz'
-    #url = 'https://noaa-nexrad-level2.s3.amazonaws.com/1999/04/05/KAMX/KAMX19990405_000408.gz'
     data = urlopen(url).read()
-    response = make_response(data)
+    script_dir = os.path.dirname(__file__)
+    rel_path = "dummy.kml"
+    abs_file_path = os.path.join(script_dir, rel_path)
+    content_data = open(abs_file_path,'r').read()
+    response = make_response(content_data)
     response.headers["Content-Disposition"] = "attachment; filename=data.kml"
     return response
 
