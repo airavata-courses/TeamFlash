c2956e831cc34447a901ca714d3dd89072dddf0d
Pranav <pmpande@iu.edu>
2016-09-25 20:49:28 -0400

    Updated URL request

--- StormDetection/StormDetection.py
+++ StormDetection/StormDetection.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import make_response
+from flask import request
 from urllib.request import urlopen
 import os
 
 app = Flask(__name__)
 
-@app.route('/detectStorm/<url>', methods=['GET'])
-def detectStormLocation(url=None):
+@app.route('/detectStorm', methods=['GET','POST'])
+def detectStormLocation():
     #url = 'https://noaa-nexrad-level2.s3.amazonaws.com/' + year + '/' + month + '/' + day + '/' + station + '/' + filename + '.gz'
-    data = urlopen(url).read()
+    url = request.args.get('url')
+    try:
+        data = urlopen(url).read()
+    except:
+        pass
     script_dir = os.path.dirname(__file__)
     rel_path = "dummy.kml"
     abs_file_path = os.path.join(script_dir, rel_path)
