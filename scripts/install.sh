a7ce8f7ff32ea7ad8e429ac4a9e5643de837ecde
Pranav Pande <pmpande@iu.edu>
2016-09-26 18:01:55 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,8 @@ echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/teamfl
 pip install virtualenv >> /var/log/teamflash-stormclustering-install.log
 virtualenv venv >> /var/log/teamflash-stormclustering-install.log
 . venv/bin/activate >> /var/log/teamflash-stormclustering-install.log
-pip install requests
+pip install requests >> /var/log/teamflash-stormclustering-install.log
+pip install request >> /var/log/teamflash-stormclustering-install.log
 pip install Flask >> /var/log/teamflash-stormclustering-install.log
 export FLASK_APP=StormClustering.py
 flask run --host=0.0.0.0 --port=8000 >> /var/log/teamflash-stormclustering-server.log 2>&1 &
@@ -15,7 +16,8 @@ echo 'Activating virtualenv for StormDetection Microservice' >> /var/log/teamfla
 pip install virtualenv >> /var/log/teamflash-stormdetection-install.log
 virtualenv venv >> /var/log/teamflash-stormdetection-install.log
 . venv/bin/activate >> /var/log/teamflash-stormdetection-install.log
-pip install requests
+pip install requests >> /var/log/teamflash-stormdetection-install.log
+pip install request >> /var/log/teamflash-stormdetection-install.log
 pip install Flask >> /var/log/teamflash-stormdetection-install.log
 export FLASK_APP=StormDetection.py
 flask run --host=0.0.0.0 --port=7000 >> /var/log/teamflash-stormdetection-server.log 2>&1 &
