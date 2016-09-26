0ef1b7b61078345ef3faecb4690d55ee4a3eab47
Pranav <pmpande@iu.edu>
2016-09-26 16:32:30 -0400

    created new branch for python services deployment

---
+++ scripts/install.sh
@@ -0,0 +1,22 @@
+echo 'starting installation process' >> /var/log/teamflash-stormclustering-install.log
+cd '/home/ec2-user/StormClustering/PythonServicesDeploy/StormClustering'
+echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/teamflash-stormclustering-install.log
+pip install virtualenv >> /var/log/teamflash-stormclustering-install.log
+virtualenv venv >> /var/log/teamflash-stormclustering-install.log
+. venv/bin/activate >> /var/log/teamflash-stormclustering-install.log
+pip install requests
+pip install Flask >> /var/log/teamflash-stormclustering-install.log
+export FLASK_APP=StormClustering.py
+flask run --host=0.0.0.0 --port=8000 >> /var/log/teamflash-stormclustering-server.log 2>&1 &
+
+echo 'starting installation process' >> /var/log/teamflash-stormdetection-install.log
+cd '/home/ec2-user/PythonServicesDeploy/StormDetection'
+echo 'Activating virtualenv for StormDetection Microservice' >> /var/log/teamflash-stormdetection-install.log
+pip install virtualenv >> /var/log/teamflash-stormdetection-install.log
+virtualenv venv >> /var/log/teamflash-stormdetection-install.log
+. venv/bin/activate >> /var/log/teamflash-stormdetection-install.log
+pip install requests
+pip install Flask >> /var/log/teamflash-stormdetection-install.log
+export FLASK_APP=StormDetection.py
+flask run --host=0.0.0.0 --port=7000 >> /var/log/teamflash-stormdetection-server.log 2>&1 &
+
