954340c0ca01f963d41b1cd9dcb473c51ce6593c
akmehra <abhishek.mehra111@gmail.com>
2016-09-25 22:43:54 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 echo 'Installing ForecastTrigger' 
 cd '/home/ec2-user/ForecastTrigger'
 mvn -e clean install >> /var/log/ForecastTrigger.log
-cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
-cd  /usr/local/tomcat7/apache-tomcat-7.0.72
+cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/tomcat.log
+cd  /usr/share/tomcat7
 
-sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
+sudo service tomcat7 start >> /var/log/tomcat.log 2>&1 &
