ab3b1f484bc7ebe177cd04083d1f28f4d816acf2
akmehra <abhishek.mehra111@gmail.com>
2016-09-26 18:19:43 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,6 @@ echo 'Installing ForecastTrigger'
 cd '/home/ec2-user/StormCheck'
 mvn -e clean install >> /var/log/ForecastTrigger.log
 cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/tomcat.log
-cd  /usr/share/tomcat7
+cd  /usr/share/tomcat7/bin
 
-sudo service tomcat7 start >> /var/log/tomcat.log 2>&1 &
+sh ./start.sh >> /var/log/tomcat.log 2>&1 &
