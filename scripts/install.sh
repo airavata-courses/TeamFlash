dbadd05e8c12f65ab4ba65b7ad07f861c14972ab
akmehra <abhishek.mehra111@gmail.com>
2016-09-26 22:17:45 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 echo 'Installing RunWeatherForecast' 
-cd '/home/ec2-user/RunForecast'
+cd '/home/ec2-user/RunForecast/RunForecast'
 mvn -e clean install >> /var/log/RunForecast.log
 cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/tomcat.log
 cd  /usr/share/tomcat7/bin
