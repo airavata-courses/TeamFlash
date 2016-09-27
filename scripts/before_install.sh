931798bf4a47364ae8590f5c70befb1a6b95e6b7
Pranav Pande <pmpande@iu.edu>
2016-09-27 00:44:25 -0400

    updated

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,5 @@
 echo 'killing existing node process if any running' >> /var/log/node-before.log
-ps -ef | grep node >> /var/log/node-before.log
-pkill -f node >> /var/log/node-before.log
+#ps -ef | grep node >> /var/log/node-before.log
+#pkill -f node >> /var/log/node-before.log
+killall node >> /var/log/node-before.log
 sleep 5
