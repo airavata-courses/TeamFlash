e727dcd1047f8bdfad624ee259b1a24e167c00ec
akmehra <akmehra@iu.edu>
2016-09-27 00:49:41 -0400

    Continuous deployment for Registry

---
+++ scripts/before-install.sh
@@ -0,0 +1,11 @@
+echo 'Setting up our java environment'
+#export JAVA_HOME=/usr/java/jdk1.8.0_45/jre
+echo 'check if maven is installed'
+mvn --version
+if [ "$?" -ne 0 ]; then
+    echo 'Installing Maven...'
+	sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+	sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+	sudo yum install -y apache-maven
+	mvn --version
+fi
---
+++ scripts/install.sh
@@ -0,0 +1,7 @@
+
+echo 'Installing Registry' 
+cd '/home/ec2-user/Registry/Registry'
+mvn clean install >> /var/log/Registry.log
+cd 'target'
+chmod 777 *
+java -jar Registry*.jar 8767 >> /var/log/Registry.log 2>&1 &
