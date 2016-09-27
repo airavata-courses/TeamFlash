73425bcc0a734d078fc1aa55a87dbc2ca55cf653
akmehra <abhishek.mehra111@gmail.com>
2016-09-27 00:25:54 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,4 +3,4 @@ cd '/home/ec2-user/DataIngestor/DataIngestor'
 mvn clean install >> /var/log/DataIngestor.log
 cd 'target'
 chmod 777 *
-java -jar *.jar 2345 >> /var/log/DataIngestor.log 2>&1 &
+java -jar Data*.jar 8765 >> /var/log/DataIngestor.log 2>&1 &
