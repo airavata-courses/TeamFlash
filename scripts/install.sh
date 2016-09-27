95591f0e9febcef490eaba0dd6de624390e9ab8e
akmehra <abhishek.mehra111@gmail.com>
2016-09-27 04:35:30 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,7 @@
 echo 'Installing Registry' 
 cd '/home/ec2-user/Registry/Registry'
 mvn clean install >> /var/log/Registry.log
+cp config.properties target/
 cd 'target'
 chmod 777 *
 java -jar Registry*.jar 8800 >> /var/log/Registry1.log 2>&1 &
