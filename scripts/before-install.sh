497c9651da70844bb10d1146da6b841a642c1021
akmehra <akmehra@iu.edu>
2016-09-26 22:04:54 -0400

    Continuous Integration

---
+++ scripts/before-install.sh
@@ -0,0 +1,15 @@
+echo 'killing existing tomcat process if any'
+cd /usr/share/tomcat7/bin
+sh ./stop.sh
+sleep 20
+echo 'Setting up our java environment'
+#export JAVA_HOME=/usr/java/jdk1.8.0_45/jre
+echo 'check if maven is installed'
+mvn --version
+if [ "$?" -ne 0 ]; then
+    echo 'Installing Maven...'
+	sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+	sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+	sudo yum install -y apache-maven
+	mvn --version
+fi
---
+++ scripts/install.sh
@@ -0,0 +1,7 @@
+echo 'Installing RunWeatherForecast' 
+cd '/home/ec2-user/RunForecast'
+mvn -e clean install >> /var/log/RunForecast.log
+cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/tomcat.log
+cd  /usr/share/tomcat7/bin
+
+sh ./start.sh >> /var/log/tomcat.log 2>&1 &
