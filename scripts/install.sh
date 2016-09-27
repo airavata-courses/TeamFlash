af3f0de13e16e52c4c190528e0e49efec0f10388
Girish Gabra <girish.gabra10@gmail.com>
2016-09-27 02:47:53 -0400

    change port number

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,4 @@ cd '/home/ec2-user/Registry/Registry'
 mvn clean install >> /var/log/Registry.log
 cd 'target'
 chmod 777 *
-java -jar Registry*.jar 8767 >> /var/log/Registry1.log 2>&1 &
+java -jar Registry*.jar 8800 >> /var/log/Registry1.log 2>&1 &
