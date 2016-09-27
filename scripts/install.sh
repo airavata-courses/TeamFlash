d1e0aa7df45035bf2c067c932205a80e0f3eec84
Pranav Pande <pmpande@iu.edu>
2016-09-26 23:40:19 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,6 @@ cd '/home/ec2-user/Gateway/Gateway' >> /var/log/teamflash-gateway-install.log
 echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/teamflash-gateway-install.log
 npm install mongodb >> /var/log/teamflash-gateway-install.log
 npm link mongodb >> /var/log/teamflash-gateway-install.log
+npm install node-uuid >> /var/log/teamflash-gateway-install.log
 node index.js >> /var/log/teamflash-gateway-server.log 2>&1 &
 mongod >> /var/log/teamflash-gateway-db.log 2>&1 &
