84a291a39edf4756e30790135af4ebda01c629a8
akmehra <abhishek.mehra111@gmail.com>
2016-09-27 19:24:38 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,4 +5,4 @@ mvn clean install >> /var/log/Registry.log
 cp config.properties target/
 cd 'target'
 chmod 777 *
-java -jar Registry*.jar 8800 >> /var/log/Registry1.log 2>&1 &
+java -jar Registry*.jar 8550 >> /var/log/Registry1.log 2>&1 &
