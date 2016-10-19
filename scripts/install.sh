ef2d779511a98320cf038d0347721b2357372f86
akmehra <abhishek.mehra111@gmail.com>
2016-10-19 03:14:31 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,8 @@
 echo 'Installing ForecastTrigger' 
 cd '/home/ec2-user/ForecastTrigger/StormCheck'
 mvn -e clean install >> /var/log/ForecastTrigger.log
-cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/tomcat.log
-cd  /usr/share/tomcat7/bin
-
-sh ./start.sh >> /var/log/tomcat.log 2>&1 &
+#cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/tomcat.log
+#cd  /usr/share/tomcat7/bin
+docker build -t forecastTrigger .
+docker run -it --rm -p 8080:8080 --name StormCheck forecastTrigger >> /var/log/forecastTriggerDocker.log 2>&1 &
+#sh ./start.sh >> /var/log/tomcat.log 2>&1 &
