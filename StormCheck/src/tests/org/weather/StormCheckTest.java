c9366b161d21561f6a3f5ce9265cbc50a890b82f
Girish Gabra <girish.gabra10@gmail.com>
2016-10-19 04:20:36 -0400

    Update test root folder path

---
+++ StormCheck/src/tests/org/weather/StormCheckTest.java
@@ -0,0 +1,20 @@
+package org.weather;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by girish on 10/19/16.
+ */
+public class StormCheckTest {
+    @Test
+    public void checkStormExists() throws Exception {
+
+        StormCheck stormcheck = new StormCheck();
+        String res = stormcheck.checkStormExists(false);
+        assertTrue(res!=null);
+    }
+    
+}
