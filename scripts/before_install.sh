a117fada0963e5815fcbce3fc2eb2aca323f0bb4
Pranav Pande <pmpande@iu.edu>
2016-10-19 20:12:23 -0400

    build test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 echo 'killing existing flask process if any running' >> /var/log/flask-before-clustering.log
-#ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
-kill $(lsof -t -i:8000) >> /var/log/flask-before-clustering.log
+ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
+#kill $(lsof -t -i:8000) >> /var/log/flask-before-clustering.log
 sleep 5
