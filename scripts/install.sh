75bffeb1cb9d084324b068d98b5ea60439b3a5c5
akmehra <abhishek.mehra111@gmail.com>
2016-10-20 13:12:47 -0400

    update with no dangling

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,6 @@ mvn -e clean install >> /var/log/ForecastTrigger.log
 #cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/tomcat.log
 #cd  /usr/share/tomcat7/bin
 docker build -t forecasttrigger .
+docker rmi -f $(docker images -f "dangling=true" -q)
 docker run -p 8080:8080 --name StormCheck forecasttrigger >> /var/log/forecastTriggerDocker.log 2>&1 &
 #sh ./start.sh >> /var/log/tomcat.log 2>&1 &
