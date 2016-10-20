afdf2bd829bfbab81f83a2a86e96ae32a7282a1a
Pranav Pande <pmpande@iu.edu>
2016-10-19 20:22:52 -0400

    build test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,12 @@
 echo 'starting installation process' >> /var/log/teamflash-stormclustering-install.log
 cd '/home/ec2-user/StormClustering/StormClustering'
-
 echo 'Activating virtualenv for StormClustering Microservice' >> /var/log/teamflash-stormclustering-install.log
 pip install virtualenv >> /var/log/teamflash-stormclustering-install.log
 virtualenv venv >> /var/log/teamflash-stormclustering-install.log
 . venv/bin/activate >> /var/log/teamflash-stormclustering-install.log
 pip install requests >> /var/log/teamflash-stormclustering-install.log
+pip install request >> /var/log/teamflash-stormclustering-install.log
 pip install Flask >> /var/log/teamflash-stormclustering-install.log
-pip install nose >> /var/log/teamflash-stormclustering-install.log
 export FLASK_APP=StormClustering.py
 flask run --host=0.0.0.0 --port=8000 >> /var/log/teamflash-stormclustering-server.log 2>&1 &
 
