2729e1e51101d00587afe3fe08d4fb1904a8d331
Girish Gabra <girish.gabra10@gmail.com>
2016-10-19 20:00:52 -0400

    Junit changes

--- RunForecast/src/main/java/org/weather/RunWeatherForecast.java
+++ RunForecast/src/main/java/org/weather/RunWeatherForecast.java
@@ -11,15 +11,10 @@ public class RunWeatherForecast {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-<<<<<<< HEAD
-    public String runForecast(@QueryParam("location") String locationName) {
 
-        if(locationName==null || locationName.length()==0){
-=======
     public String runForecast(@QueryParam("location") String locationName)
     {
         if(locationName==null || locationName.length()==0 ){
->>>>>>> 9238f8234d2c59dfe68e56a930dce9b8fd5d0b84
             throw new IllegalArgumentException();
         }
 
@@ -27,11 +22,7 @@ public class RunWeatherForecast {
         location.setLocationName(locationName);
         location.setTemperature(10.0);
         location.setWindSpeed(12.5);
-<<<<<<< HEAD
-        Gson gson = new Gson();
-=======
         Gson gson =  new Gson();
->>>>>>> 9238f8234d2c59dfe68e56a930dce9b8fd5d0b84
         //System.out.println("Before fun call");
         // LocationDAO locationDAO = new LocationDAO();
         // locationDAO.getWeatherInfo(locationid);
--- RunForecast/src/test/org/weather/TestRunWeatherForecast.java
+++ RunForecast/src/test/org/weather/TestRunWeatherForecast.java
@@ -1,24 +1,18 @@
 package org.weather;
 
 import static org.junit.Assert.*;
-<<<<<<< HEAD
 import org.junit.*;
-=======
+
 import org.junit.Assert;
 import org.junit.Test;
 
->>>>>>> 9238f8234d2c59dfe68e56a930dce9b8fd5d0b84
 /**
  * Created by girish on 10/19/16.
  */
 public class TestRunWeatherForecast {
     @Test(expected = IllegalArgumentException.class)
-<<<<<<< HEAD
     public void testArgumentNull() throws Exception {
 
-=======
-    public void testNull() throws Exception {
->>>>>>> 9238f8234d2c59dfe68e56a930dce9b8fd5d0b84
         RunWeatherForecast runWeatherForecast = new RunWeatherForecast();
         runWeatherForecast.runForecast(null);
     }
@@ -27,14 +21,13 @@ public class TestRunWeatherForecast {
     public void testOutput()
     {
         RunWeatherForecast runWeatherForecast = new RunWeatherForecast();
-<<<<<<< HEAD
         String res = runWeatherForecast.runForecast("Spain");
         Assert.assertTrue(res!=null);
-    }
-=======
+
+
         String str = runWeatherForecast.runForecast("Spain");
         Assert.assertTrue(str!=null);
     }
 
->>>>>>> 9238f8234d2c59dfe68e56a930dce9b8fd5d0b84
+
 }
