20514afa8b692e7f2cca3a6dae99f7314d131177
akmehra <abhishek.mehra111@gmail.com>
2016-10-20 13:32:36 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,6 @@ mvn -e clean install >> /var/log/ForecastTrigger.log
 #cd  /usr/share/tomcat7/bin
 docker build -t forecasttrigger .
 docker rmi -f $(docker images -f "dangling=true" -q)
-docker run -p 8080:8080 --name StormCheck forecasttrigger >> /var/log/forecastTriggerDocker.log 2>&1 &
+docker run -p 8080:8080 --name StormChecklatest forecasttrigger >> /var/log/forecastTriggerDocker.log 2>&1 &
 
 #sh ./start.sh >> /var/log/tomcat.log 2>&1 &
