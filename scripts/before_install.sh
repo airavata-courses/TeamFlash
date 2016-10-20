413f4a90f64d3d77cf257fcf533ab96957d7da04
Pranav Pande <pmpande@iu.edu>
2016-10-20 04:35:52 -0400

    build test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,6 @@
 echo 'killing existing flask process if any running' >> /var/log/flask-before-clustering.log
-if [[ "$(docker images -q myimage:mytag 2> /dev/null)" == "" ]]; then
-docker stop stormclustering
-docker rm -f stormclustering
-else
-echo "No container present"
-fi
+docker ps -a | grep 'stormclustering' | awk '{print $1}' | xargs --no-run-if-empty docker stop >> /var/log/flask-before-clustering.log
+docker ps -a | grep 'stormclustering' | awk '{print $1}' | xargs --no-run-if-empty docker rm >> /var/log/flask-before-clustering.log
 #ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 #kill $(lsof -t -i:8000) >> /var/log/flask-before-clustering.log
 #sleep 5
