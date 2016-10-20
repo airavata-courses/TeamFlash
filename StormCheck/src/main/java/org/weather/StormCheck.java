b112c522da3063bb1495440e55b9ad25055f1000
Girish Gabra <girish.gabra10@gmail.com>
2016-10-20 19:36:50 -0400

    Added code for load testing

--- StormCheck/src/main/java/org/weather/StormCheck.java
+++ StormCheck/src/main/java/org/weather/StormCheck.java
@@ -2,7 +2,7 @@ package org.weather;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-
+import java.util.*;
 /**
  * Created by girish on 9/18/16.
  */
@@ -17,10 +17,36 @@ public class StormCheck {
     {
 
         System.out.println(exists);
+        long start = System.currentTimeMillis();
+        doCapactiyTesting();
+
         if(!exists){
             isStormPresent = "{\"message\": \"No\"}";
         }
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time: "+(end-start));
 
         return isStormPresent;
+    }
+
+    private void doCapactiyTesting()
+    {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int result=0;
+        for(int i=0;i<200000;i++)
+        {
+            list.add(i);
+            result+=Math.pow(i,10000);
+            if(result%5==0)
+            {
+                System.out.println("Divisible by 5");
+            }
+        }
+
+        for(int i=0;i<200000;i++)
+        {
+            list.remove(0);
+        }
     }
 }
