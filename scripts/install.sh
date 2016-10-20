1b48e52f334d458b4fa502dc99b936eee53a1211
akmehra <akmehra@iu.edu>
2016-10-20 12:57:44 -0400

    dockerizing RunWeather Forecast service

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,12 @@
 echo 'Installing RunWeatherForecast' 
 cd '/home/ec2-user/RunForecast/RunForecast'
+rm -f /var/log/RunForecast.log
+rm -f /var/log/forecastTriggerDocker.log
 mvn -e clean install >> /var/log/RunForecast.log
-cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/tomcat.log
-cd  /usr/share/tomcat7/bin
+#cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/tomcat.log
+#cd  /usr/share/tomcat7/bin
 
-sh ./start.sh >> /var/log/tomcat.log 2>&1 &
+docker build -t forecasttrigger .
+docker run -p 8081:8080 --name StormCheck forecasttrigger >> /var/log/forecastTriggerDocker.log 2>&1 &
+
+#sh ./start.sh >> /var/log/tomcat.log 2>&1 &
