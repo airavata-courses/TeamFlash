72976ae5671ec2b9e97c46fd2a228f92a1f0e356
akmehra <abhishek.mehra111@gmail.com>
2016-10-20 05:39:36 -0400

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 echo 'killing existing flask process if any running' >> /var/log/flask-before-clustering.log
-docker ps -a | grep 'stormclustering' | awk '{print $1}' | xargs --no-run-if-empty docker stop >> /var/log/flask-before-clustering.log
-docker ps -a | grep 'stormclustering' | awk '{print $1}' | xargs --no-run-if-empty docker rm >> /var/log/flask-before-clustering.log
+docker ps -a | grep 'StormClustering' | awk '{print $1}' | xargs --no-run-if-empty docker stop >> /var/log/flask-before-clustering.log
+docker ps -a | grep 'StormClustering' | awk '{print $1}' | xargs --no-run-if-empty docker rm >> /var/log/flask-before-clustering.log
 #ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 #kill $(lsof -t -i:8000) >> /var/log/flask-before-clustering.log
 #sleep 5
