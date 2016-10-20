5d9d9fe5ffd42df630dbfd7a4393a4f8289c0773
akmehra <abhishek.mehra111@gmail.com>
2016-10-19 23:31:29 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 echo 'Installing DataIngestor' 
 cd '/home/ec2-user/DataIngestor/DataIngestor'
 mvn clean install >> /var/log/DataIngestor.log
-cd 'target'
-chmod 777 *
-java -jar Data*.jar 8765 >> /var/log/DataIngestor.log 2>&1 &
+#cd 'target'
+#chmod 777 *
+#java -jar Data*.jar 8765 >> /var/log/DataIngestor.log 2>&1 &
+docker build -t dataingestor .
+docker run -p 8765:8765 --name DataIngestor dataingestor >> /var/log/DataIngestor.log 2>&1 &
