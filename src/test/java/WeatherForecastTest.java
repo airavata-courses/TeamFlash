a1028299d4062836b78f930b8edda577610742c0
Pranav <pmpande@iu.edu>
2016-10-21 01:40:18 -0400

    Added tests

--- src/test/java/WeatherForecastTest.java
+++ src/test/java/WeatherForecastTest.java
@@ -1,26 +1,16 @@
-/**
- * Created by pranav on 10/20/16.
- */
-
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
-
 import java.io.IOException;
-
 import io.github.bonigarcia.wdm.MarionetteDriverManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.MarionetteDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class WeatherForecastTest {
 
     private String baseUrl;
@@ -34,7 +24,7 @@ public class WeatherForecastTest {
     @Before
     public void openBrowser() {
         driver = new MarionetteDriver();
-        baseUrl = "https://www.google.com";
+        baseUrl = "http://52.55.196.63:8888/login";
         driver.get(baseUrl);
     }
 
@@ -46,17 +36,59 @@ public class WeatherForecastTest {
     }
 
     @Test
-    public void login() throws IOException {
-        assertEquals("The page title should equal Google at the start of the test.", "Google", driver.getTitle());
-        WebElement searchField = driver.findElement(By.name("q"));
-        searchField.sendKeys("Drupal!");
-        searchField.submit();
-        assertTrue("The page title should start with the search string after the search.",
-                (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-                    public Boolean apply(WebDriver d) {
-                        return d.getTitle().toLowerCase().startsWith("drupal!");
-                    }
-                })
-        );
+    public void firstTest() throws IOException {
+        WebElement submit = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.name("commit")));
+
+
+        WebElement user = driver.findElement(By.name("login"));
+        user.sendKeys("pranavpande");
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("pranavpande");
+
+        submit.submit();
+
+        WebElement element = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.id("dataIngestorButton")));
+    }
+
+    @Test
+    public void secondTest() throws IOException {
+        WebElement date = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.id("date")));
+
+        date.sendKeys("05/04/1988");
+
+        WebElement time = driver.findElement(By.name("time"));
+        time.sendKeys("00408");
+
+        new Select(driver.findElement(By.id("station"))).selectByVisibleText("Miami");
+
+        WebElement forecast = driver.findElement(By.id("dataIngestorButton"));
+        forecast.submit();
+
+        //add result check
+    }
+
+    @Test
+    public void thirdTest() throws IOException {
+        WebElement audit = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.id("auditButton")));
+
+        audit.click();
+
+        //add table check
+    }
+
+    @Test
+    public void fourthTest() throws IOException {
+        WebElement logout = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.id("logoutButton")));
+
+        logout.submit();
+
+        WebElement submit = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.name("commit")));
     }
 }
