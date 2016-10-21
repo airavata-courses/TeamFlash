249ff25aaacee6c0aab67611f0ce3250d487a17a
Pranav <pmpande@iu.edu>
2016-10-21 03:29:17 -0400

    Checking ID

--- StormClustering/StormClustering.py
+++ StormClustering/StormClustering.py
@@ -2,14 +2,28 @@ import os
 from flask import Flask
 from flask import Response
 from flask import json
-
+from flask import make_response
+from flask import request
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/detectClusters', methods=['GET'])
 def detectClusters():
-    clusters = {
+	try:
+		id = request.args.get('id')
+		print("ID:" + str(id))
+	except:
+		print("No ID")
+	
+	try:
+		user = request.args.get('username')
+		print("user:" + user)
+	except:
+		print("No user")
+	
+	clusters = {
         "data" : [
             {"longitude":"-112.0822680013139","latitude":"36.09825589333556","altitude":"0",
              "heading":"103.8120432044965","tilt":"62.04855796276328","range":"2889.145007690472"},
@@ -20,12 +34,12 @@ def detectClusters():
         ]
     }
 
-    js = json.dumps(clusters)
-    response = Response(js, status=200, mimetype='application/json')
-    return response
+	js = json.dumps(clusters)
+	response = Response(js, status=200, mimetype='application/json')
+	return response
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 8000))
+	app.run(host='0.0.0.0', port=port)
 
