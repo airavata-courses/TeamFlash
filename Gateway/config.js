42ed8f15ab78061f4d3cea113d8447ae8e58f9de
Debasis Dwivedy <dwivedy.debasis@gmail.com>
2016-10-20 21:15:52 -0400

    Update

--- Gateway/config.js
+++ Gateway/config.js
@@ -6,6 +6,6 @@ module.exports = {
   oauth2: {
     clientId: '454533374641-7pi0orpb4u90odj6umitgsnpjj4vge1p.apps.googleusercontent.com',
     clientSecret: 'Ickb2LRup5yIKisnyydmz0Ay',
-    redirectUrl: process.env.REDIRECT_URL || 'http://localhost:8888/oauth2callback'
+    redirectUrl: process.env.REDIRECT_URL || 'http://52.55.196.63:8888/oauth2callback'
   }
 };
