3a8222dacb06224ddb8c2906f49aadb67ad18d6e
Pranav <pmpande@iu.edu>
2016-10-26 23:26:57 -0400

    updated direc name

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,11 @@
-echo 'Installing RunWeatherForecast' 
-cd '/home/ec2-user/RunForecast/RunForecast'
+echo 'Installing RunWeatherForecast'
+
+rm -r /home/ec2-user/runForecast
+mv /home/ec2-user/RunForecast  /home/ec2-user/runForecast
+
+cd /home/ec2-user/runForecast/
+cd RunForecast
+
 rm -f /var/log/RunForecast.log
 rm -f /var/log/forecastTriggerDocker.log
 mvn -e clean install >> /var/log/RunForecast.log
