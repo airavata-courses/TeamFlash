12cf51cf58541af2d1ec63aaf1361e4997ca77fc
Pranav <pmpande@iu.edu>
2016-10-26 22:55:34 -0400

    updated direc name

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,10 @@
 echo 'starting installation process' >> /var/log/teamflash-stormdetection-docker-install.log
-cd '/home/ec2-user/StormDetection/'
+
+rm -r /home/ec2-user/stormDetection
+mv /home/ec2-user/StormDetection  /home/ec2-user/stormDetection
+
+cd /home/ec2-user/stormDetection/
+cd StormDetection
 docker build --no-cache=true -t stormdetection . >> /var/log/teamflash-stormdetection-docker-install.log
 docker rmi -f $(docker images -f "dangling=true" -q) >> /var/log/teamflash-stormdetection-docker-install.log
 docker run -p 7000:7000 --name StormDetection stormdetection >> /var/log/teamflash-stormdetection-docker-server.log 2>&1 &
@@ -14,4 +19,3 @@ docker run -p 7000:7000 --name StormDetection stormdetection >> /var/log/teamfla
 #pip install Flask >> /var/log/teamflash-stormdetection-install.log
 #export FLASK_APP=StormDetection.py
 #flask run --host=0.0.0.0 --port=7000 >> /var/log/teamflash-stormdetection-server.log 2>&1 &
-
