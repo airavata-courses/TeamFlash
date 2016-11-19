8998df4202d8b649395742d02540111564e4406c
akmehra <abhishek.mehra111@gmail.com>
2016-11-18 22:15:07 -0500

    adding logging for container replication

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,8 +10,8 @@ cd StormCheck
 mvn -e clean install >> /var/log/ForecastTrigger.log
 #cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/tomcat.log
 #cd  /usr/share/tomcat7/bin
-docker build -t forecasttrigger .
-docker rmi -f $(docker images -f "dangling=true" -q)
+docker build -t forecasttrigger . >> /var/log/forecastTriggerDocker.log
+docker rmi -f $(docker images -f "dangling=true" -q) >> /var/log/forecastTriggerDocker.log
 docker run -p 8080:8080 --name StormChecklatest forecasttrigger >> /var/log/forecastTriggerDocker.log 2>&1 &
 
 #sh ./start.sh >> /var/log/tomcat.log 2>&1 &
