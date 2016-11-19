b4794841722d41e22a8dc0ee8dfd3240bcae8838
akmehra <abhishek.mehra111@gmail.com>
2016-11-19 03:52:40 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@ echo 'Installing LoadBalancer'
 rm -r /home/ec2-user/loadBalancer
 mv /home/ec2-user/LoadBalancer /home/ec2-user/loadBalancer
 
-cd '/home/ec2-user/LoadBalancer/zookeeperManager'
+cd '/home/ec2-user/loadBalancer/zookeeperManager'
 
 mvn -e clean install >> /var/log/loadBalancer.log
 cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/LoadBalancer.log
