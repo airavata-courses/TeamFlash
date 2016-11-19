70243780f0ff1f3c710b5b28e6b1c1fa4cee8b62
Girish Gabra <girish.gabra10@gmail.com>
2016-11-19 17:26:29 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,4 +9,4 @@ mvn -e clean install >> /var/log/loadBalancer.log
 cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/LoadBalancer.log
 cd  /usr/share/tomcat7/bin
 
-sh ./start.sh >> /var/log/tomcat.log 2>&1 &
+sudo service tomcat7 start >> /var/log/tomcat.log 2>&1 &
