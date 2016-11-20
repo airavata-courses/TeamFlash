68fd1c7d0e6aea975741eaec1bcb9559d197081b
akmehra <abhishek.mehra111@gmail.com>
2016-11-19 21:40:09 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ mv /home/ec2-user/LoadBalancer /home/ec2-user/loadBalancer
 cd '/home/ec2-user/loadBalancer/LoadBalancer'
 
 mvn -e clean install >> /var/log/loadBalancer.log
-cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/LoadBalancer.log
+sudo cp target/*.war /usr/share/tomcat7/webapps/ >> /var/log/LoadBalancer.log
 cd  /usr/share/tomcat7/bin
 
 sudo service tomcat7 start >> /var/log/tomcat.log 2>&1 &
