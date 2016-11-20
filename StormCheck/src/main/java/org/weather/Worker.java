0029b67aae68662e42399e603598bf5ead3ee20e
Girish Gabra <girish.gabra10@gmail.com>
2016-11-20 03:16:31 -0500

    imported URL package to get ip

--- StormCheck/src/main/java/org/weather/Worker.java
+++ StormCheck/src/main/java/org/weather/Worker.java
@@ -11,7 +11,8 @@ import javax.servlet.annotation.WebListener;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.ext.Provider;
-
+import java.io.InputStreamReader;
+import java.net.URL;
 /**
  * Created by girish on 11/17/16.
  */
@@ -24,21 +25,15 @@ public class Worker implements ServletContextListener
     {
         //ServiceProvider<Void> serviceProvider;
         //String[] urlEndpoints = {"52.52.165.77","52.52.164.169"};
-        URL url = new URL("http://checkip.amazonaws.com");
-        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-        String ip = in.readLine();
-
         ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3);
-
         CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("52.52.144.190:2181", retryPolicy);
-
         curatorFramework.start();
-
-
-        int port = 8080;
-
         try
         {
+           URL url = new URL("http://checkip.amazonaws.com");
+           BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+           String ip = in.readLine(); 
+           int port = 8080; 
             ServiceInstance serviceInstance = ServiceInstance.builder()
                     .uriSpec(new UriSpec("{scheme}://{address}:{port}"))
                     .address(ip)
