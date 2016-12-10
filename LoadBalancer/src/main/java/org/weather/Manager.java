25d76c5f4f9fee81961039c160960172efe57ad1
akmehra <abhishek.mehra111@gmail.com>
2016-12-10 17:28:46 -0500

    Updated code

--- LoadBalancer/src/main/java/org/weather/Manager.java
+++ LoadBalancer/src/main/java/org/weather/Manager.java
@@ -267,7 +267,7 @@ public class Manager {
     public  String forecastTriggerDelegate(@QueryParam("value") boolean exists) throws Exception {
         ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3);
         //CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(Config.get().get("zk.quorum"),Config.get().getInt("zk.session.timeout", 3000),1000,retryPolicy);
-        List<String> hosts = ["52.52.144.190:2181", "52.52.165.77:2181", "52.52.164.169:2181"]
+        List<String> hosts = ["52.52.144.190:2181", "52.52.165.77:2181", "52.52.164.169:2181"];
         CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(hosts,retryPolicy);
         curatorFramework.start();
         /*for service forecast trigger*/
